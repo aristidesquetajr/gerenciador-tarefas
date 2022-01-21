7b99a6d1104b1cde4e0b696b1fd2ad0c30bfca9a
Aristides Queta AK <aristidesquetajr@gmail.com>
2022-01-21 03:58:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gerenciador de tarefas
+
+## Tecnologias
+
+Este projecto foi desenvolvido com as seguintes tecnologias da web
+
+* HTML
+* CSS
+* Javascript
+* Font Awesome
---
+++ js/app.js
@@ -0,0 +1,49 @@
+const strLocalstorage = 'items-gTarefas'
+const task = JSON.parse(localStorage.getItem(strLocalstorage)) || ["Welcome"]
+const form = document.querySelector('form')
+const ul = document.getElementById('list-items')
+
+rendererTasks()
+
+function rendererTasks() {
+    ul.innerText = ''
+    
+    for(let index = task.length - 1; index >= 0; index--) {
+        const li = document.createElement('li')
+
+        let output = `<span>${task[index]}</span>`
+        output += `<span class="icons"> <span class="icon" onclick="editTask(${index})"><i class="far fa-edit"></i></span>`
+        output += `&nbsp; <span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></span>`
+
+        li.innerHTML = output
+        ul.appendChild(li)
+    }
+}
+
+function addTask() {
+    let text = document.querySelector('#idTarefa')
+    if (text.value.length) {
+        task.push(text.value)
+        text.value = ''
+        saveLocalStorage()
+    } else window.alert("Digite uma tarefa")
+}
+
+function saveLocalStorage() {
+    localStorage.setItem(strLocalstorage, JSON.stringify(task))
+    rendererTasks()
+}
+
+function deleteTask(index) {
+    task.splice(index, 1)
+    saveLocalStorage()
+}
+
+function editTask(index) {
+    let newText = window.prompt("Informe o texto")
+}
+
+form.addEventListener('submit', () => {
+    event.preventDefault()
+    addTask()
+})
