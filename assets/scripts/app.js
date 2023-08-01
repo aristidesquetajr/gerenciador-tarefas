07bdeb8466a86ec2e32a3b577b8a568e9499fd79
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-01 22:14:15 +0100

    added: edit function task

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 const strLocalstorage = 'items-gTarefas'
-const task = JSON.parse(localStorage.getItem(strLocalstorage)) ?? ["Welcome"]
+const tasks = JSON.parse(localStorage.getItem(strLocalstorage)) ?? ["Welcome"]
 const form = document.querySelector('form')
 const ul = document.getElementById('list-items')
 
@@ -7,15 +7,16 @@ rendererTasks()
 
 function rendererTasks() {
     ul.innerText = ''
-    task.forEach((element, index) => {
+    tasks.forEach((element, index) => {
         const li = document.createElement('li')
         const icons = document.createElement('span')
         icons.classList.add('icons')
         
         li.classList.add(`task-${index}`)
-        li.innerHTML = `<span class="task-text">${element}</span>`
-        icons.appendChild(iconEdit(index))
+        li.innerHTML = `<span class="task-text" contenteditable onkeypress="editTask(${index})">${element}</span>`
+
         icons.appendChild(iconDelete(index))
+
         li.appendChild(icons)
         ul.appendChild(li)
     })
@@ -24,43 +25,36 @@ function rendererTasks() {
 function addTask() {
     let text = document.querySelector('#idTarefa')
     if (text.value.length) {
-        task.unshift(text.value)
+        tasks.unshift(text.value)
         text.value = ''
+
         saveLocalStorage()
     } else window.alert("Digite uma tarefa")
 }
 
 function saveLocalStorage() {
-    localStorage.setItem(strLocalstorage, JSON.stringify(task))
+    localStorage.setItem(strLocalstorage, JSON.stringify(tasks))
+
     rendererTasks()
 }
 
 function deleteTask(index) {
-    let isconfirm = window.confirm(`Deseja apagar a tarefa "${task[index]}"?`)
+    let isconfirm = window.confirm(`Deseja apagar a tarefa?`)
     if(isconfirm) {
-        task.splice(index, 1)
+        tasks.splice(index, 1)
+
         saveLocalStorage()
     }
 }
 
 function editTask(index) {
-    /* const taskText = document.querySelector(`li.task-${index} span.task-text`)
-    taskText.setAttribute('contenteditable', '') */ // Pensando
-    let newText = window.prompt("Informe o texto")
-    if(newText != undefined) {
-        task[index] = newText
+    const { keyCode } = event
+    const { textContent } = event.target
+
+    if(keyCode === 13) {
+        tasks[index] = textContent
         saveLocalStorage()
     }
-}
-
-function iconEdit(index) {
-    const span = document.createElement('span')
-    const i = document.createElement('i')
-    i.classList.add("far", "fa-edit")
-    i.setAttribute('onclick', `editTask(${index})`)
-    span.appendChild(i)
-    span.classList.add('icon')
-    return span
 }
 
 function iconDelete(index) {
